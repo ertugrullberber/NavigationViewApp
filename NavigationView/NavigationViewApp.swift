e3ce8cd6b397dfca4aa84d1bdb313352cda2adbe
ertugrullberber <ertugrulberber3461@gmail.com>
2022-07-22 16:41:24 +0300

    Initial Commit

---
+++ NavigationView/NavigationViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationViewApp.swift
+//  NavigationView
+//
+//  Created by Ertugrul Berber on 22.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
